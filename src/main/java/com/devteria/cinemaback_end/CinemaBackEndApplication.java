f4ac3efe4d62bd984050a30c5f4daa8b689488b1
Đinh Huỳnh Nguyên Khang <nguyenekhang0106@gmail.com>
2026-04-04 21:09:04 +0700

    mới tạo

---
+++ src/main/java/com/devteria/cinemaback_end/CinemaBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.devteria.cinemaback_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devteria/cinemaback_end/CinemaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devteria.cinemaback_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
